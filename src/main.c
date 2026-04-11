040352f551f8b9ed221aacfa21b97412dd33f73a
SJ <70745640+AIBappa@users.noreply.github.com>
2026-04-11 11:42:12 +0530

    USB updates for waht?

--- src/main.c
+++ src/main.c
@@ -12,21 +12,6 @@ int main(void) {
     int err;
     int16_t sample_buffer;
 
-    /* Initialize USB Console on the XIAO */
-    const struct device *dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
-    uint32_t dtr = 0;
-
-    if (usb_enable(NULL)) {
-        return 0;
-    }
-
-    /* Wait for a terminal to connect via USB */
-    while (!dtr) {
-        uart_line_ctrl_get(dev, UART_LINE_CTRL_DTR, &dtr);
-        k_sleep(K_MSEC(100));
-    }
-    k_sleep(K_MSEC(1000)); // Brief pause to let the terminal software catch up
-
     printk("====================================\n");
     printk("       ADS1115 SENSOR READER        \n");
     printk("====================================\n");
